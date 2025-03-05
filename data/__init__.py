7b5642ddf2597f061c324936093014f55b1735a7
Thevenin-Jonathan <warzonefury@hotmail.fr>
2025-03-05 20:33:05 +0100

    Suppression des classes PlexUser et PlexSession pour alléger le code

--- data/__init__.py
+++ data/__init__.py
@@ -1,10 +1,7 @@
 from data.database import PlexPatrolDB, load_stats, save_stats, update_user_stats
-from data.models import PlexUser, PlexSession
 
 __all__ = [
     "PlexPatrolDB",
-    "PlexUser",
-    "PlexSession",
     "load_stats",
     "save_stats",
     "update_user_stats",
