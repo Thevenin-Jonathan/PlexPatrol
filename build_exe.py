2cc5328156f8f90e643cb461d8d718afd633e620
Thevenin-Jonathan <warzonefury@hotmail.fr>
2025-03-08 18:39:10 +0100

    Ajout de fichiers de configuration pour PyInstaller et création d'un script de construction pour l'application PlexPatrol

---
+++ build_exe.py
@@ -0,0 +1,31 @@
+import PyInstaller.__main__
+import os
+
+# Chemin vers l'icône de l'application (créez ou utilisez une icône existante)
+icon_path = os.path.join("assets", "plexpatrol.ico")
+
+# Répertoires à inclure
+data_dirs = [
+    ("assets", "assets"),  # Incluez tous les fichiers de ressources
+]
+
+# Paramètres PyInstaller
+PyInstaller.__main__.run(
+    [
+        "main.py",  # Script principal
+        "--name=PlexPatrol",  # Nom de l'exécutable
+        "--onefile",  # Créer un seul fichier exécutable
+        "--windowed",  # Application GUI sans console
+        f"--icon={icon_path}",  # Icône de l'application
+        "--add-data=config;config",  # Incluez le dossier config
+        "--add-data=assets;assets",  # Incluez le dossier resources
+        "--add-data=utils;utils",  # Ajout du dossier utils
+        "--add-data=data;data",  # Ajout du dossier data si nécessaire
+        "--add-data=ui;ui",  # Ajout du dossier ui
+        "--add-data=core;core",  # Ajout du dossier core
+        "--hidden-import=utils.constants",  # Important: ajout explicite de ce module
+        "--hidden-import=config.config_manager",
+        "--noconfirm",  # Ne pas demander de confirmation
+        "--clean",  # Nettoyer avant la construction
+    ]
+)
